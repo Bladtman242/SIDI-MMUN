4b082a44d0ddbdf92984d5d4ea1df253318971d8
Michael Storgaard <github@michaelstorgaard.com>
2012-09-06 14:51:41 +0200

    Added test template and finished logic of Game of Life

--- BDSA/AS.36/GoL/GoL/GoL/Program.cs
+++ BDSA/AS.36/GoL/GoL/GoL/Program.cs
@@ -8,47 +8,148 @@ namespace GoL
     class GameOfLife
     {
         private int?[,] Grid;
+        private Random random = new Random(); // Avoid instantiating a LOT of times
+
+        // Constants for different states
+        private static readonly int? Zombie = null;
+        private static readonly int? Dead = 0;
+        private static readonly int? Alive = 1;
 
         public GameOfLife(int size)
         {
             Grid = new int?[size, size];
             GenerateState();
+        }
+
+        /// <summary>
+        /// Generate a random state
+        /// </summary>
+        private void GenerateState()
+        {
+            int size = Grid.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int? temp = random.Next(3);
+                    Grid[i, j] = (temp == 2 ? Zombie : temp);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Run the game
+        /// </summary>
+        public void Run()
+        {
+            PrintGrid();
 
             ConsoleKeyInfo cki;
             while (true)
             {
                 cki = Console.ReadKey();
+                Console.WriteLine();
                 if (cki.Key == ConsoleKey.Q)
                 {
                     break;
                 }
                 else
                 {
-                    Console.WriteLine();
                     NextDay();
-                }
-            }
-        }
-
-        private void GenerateState()
-        {
-            int size = Grid.GetLength(0);
-            Random random = new Random();
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    int? temp = random.Next(3);
-                    Grid[i, j] = (temp == 2 ? null : temp);
                 }
             }
         }
 
         private void NextDay()
         {
+            List<CellUpdate> cellUpdateList = new List<CellUpdate>();
+
+            int size = Grid.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    cellUpdateList.Add(GetNewCellState(i, j));
+                }
+            }
+
+            CellUpdate[] cellUpdates = cellUpdateList.ToArray();
+            ChangeCellStates(cellUpdates);
             PrintGrid();
         }
 
+        /// <summary>
+        /// Find new state of a cell based on neighbors
+        /// </summary>
+        /// <param name="x">X coordinate of cell</param>
+        /// <param name="y">Y coordinate of cell</param>
+        /// <returns>CellUpdate struct with new state</returns>
+        private CellUpdate GetNewCellState(int x, int y)
+        {
+            int size = Grid.GetLength(0);
+
+            int zombieN = 0;
+            int deadN = 0;
+            int liveN = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                if (i < 0 || i >= size) break; // If out of bounds, break
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (j < 0 || j >= size || (i == x && j == y)) break; // If out of bounds or self, break
+                    if (Grid[i, j] == Zombie) ++zombieN;
+                    if (Grid[i, j] == Dead) ++deadN;
+                    if (Grid[i, j] == Alive) ++liveN;
+                }
+            }
+
+            int? state = Grid[x, y];
+            if (Grid[x, y] == Alive && liveN <= 1) state = Dead;                                // 1. A live cell with 1 or less neighbors dies
+            if (Grid[x, y] == Alive && liveN >= 2 && liveN <= 3) state = Alive;                 // 2. A live cell with 2 or 3 neighbors survives
+            if (Grid[x, y] == Alive && liveN >= 4) state = Dead;                                // 3. A live cell with 4 or more neighbors dies
+            if (Grid[x, y] == Alive && zombieN >= 1 && random.Next(2) == 1) state = Zombie;     // 4. A live cell with 1 or more zombie neighbors has a 50% chance of surviving or becoming zombie
+            if (Grid[x, y] == Dead && liveN == 3) state = Alive;                                // 5. A dead cell with 3 neighbors is come to life
+
+            return new CellUpdate(x, y, state);
+        }
+        
+        /// <summary>
+        /// Change cell states according to each struct
+        /// </summary>
+        /// <param name="cellUpdates">Array of CellUpdate structs</param>
+        public void ChangeCellStates(params CellUpdate[] cellUpdates)
+        {
+            int length = cellUpdates.Length;
+            for (int i = 0; i < length; i++)
+            {
+                ChangeCellState(cellUpdates[i]);
+            }
+        }
+
+        /// <summary>
+        /// Change cell state according to struct
+        /// </summary>
+        /// <param name="cellUpdate">Cellupdate struct</param>
+        private void ChangeCellState(CellUpdate cellUpdate)
+        {
+            int size = Grid.GetLength(0);
+            if (cellUpdate.x >= size || cellUpdate.y >= size)
+            {
+                Console.WriteLine("Out of bounds");
+            }
+            else if (cellUpdate.state > 1)
+            {
+                Console.WriteLine("Unknown state");
+            }
+            else
+            {
+                Grid[cellUpdate.x, cellUpdate.y] = cellUpdate.state;
+            }
+        }
+
+        /// <summary>
+        /// Print current cell grid
+        /// </summary>
         public void PrintGrid()
         {
             int size = Grid.GetLength(0);
@@ -73,6 +174,21 @@ namespace GoL
         static void Main(string[] args)
         {
             GameOfLife gol = new GameOfLife(5);
+            gol.Run();
+        }
+    }
+
+    struct CellUpdate
+    {
+        public readonly int x;
+        public readonly int y;
+        public readonly int? state;
+
+        public CellUpdate(int x, int y, int? state)
+        {
+            this.x = x;
+            this.y = y;
+            this.state = state;
         }
     }
 }
---
+++ BDSA/AS.36/GoL/GoL/TestGameOfLife/GameOfLifeTest.cs
@@ -0,0 +1,183 @@
+﻿using GoL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestGameOfLife
+{
+    
+    
+    /// <summary>
+    ///This is a test class for GameOfLifeTest and is intended
+    ///to contain all GameOfLifeTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class GameOfLifeTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GameOfLife Constructor
+        ///</summary>
+        [TestMethod()]
+        public void GameOfLifeConstructorTest()
+        {
+            int size = 0; // TODO: Initialize to an appropriate value
+            GameOfLife target = new GameOfLife(size);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for ChangeCellState
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GoL.exe")]
+        public void ChangeCellStateTest()
+        {
+            // Private Accessor for ChangeCellState is not found. Please rebuild the containing project or run the Publicize.exe manually.
+            Assert.Inconclusive("Private Accessor for ChangeCellState is not found. Please rebuild the containing " +
+                    "project or run the Publicize.exe manually.");
+        }
+
+        /// <summary>
+        ///A test for ChangeCellStates
+        ///</summary>
+        [TestMethod()]
+        public void ChangeCellStatesTest()
+        {
+            int size = 0; // TODO: Initialize to an appropriate value
+            GameOfLife target = new GameOfLife(size); // TODO: Initialize to an appropriate value
+            CellUpdate[] cellUpdates = null; // TODO: Initialize to an appropriate value
+            target.ChangeCellStates(cellUpdates);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for GenerateState
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GoL.exe")]
+        public void GenerateStateTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            GameOfLife_Accessor target = new GameOfLife_Accessor(param0); // TODO: Initialize to an appropriate value
+            target.GenerateState();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for GetNewCellState
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GoL.exe")]
+        public void GetNewCellStateTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            GameOfLife_Accessor target = new GameOfLife_Accessor(param0); // TODO: Initialize to an appropriate value
+            int x = 0; // TODO: Initialize to an appropriate value
+            int y = 0; // TODO: Initialize to an appropriate value
+            CellUpdate_Accessor expected = null; // TODO: Initialize to an appropriate value
+            CellUpdate_Accessor actual;
+            actual = target.GetNewCellState(x, y);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Main
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GoL.exe")]
+        public void MainTest()
+        {
+            string[] args = null; // TODO: Initialize to an appropriate value
+            GameOfLife_Accessor.Main(args);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for NextDay
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GoL.exe")]
+        public void NextDayTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            GameOfLife_Accessor target = new GameOfLife_Accessor(param0); // TODO: Initialize to an appropriate value
+            target.NextDay();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for PrintGrid
+        ///</summary>
+        [TestMethod()]
+        public void PrintGridTest()
+        {
+            int size = 0; // TODO: Initialize to an appropriate value
+            GameOfLife target = new GameOfLife(size); // TODO: Initialize to an appropriate value
+            target.PrintGrid();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for Run
+        ///</summary>
+        [TestMethod()]
+        public void RunTest()
+        {
+            int size = 0; // TODO: Initialize to an appropriate value
+            GameOfLife target = new GameOfLife(size); // TODO: Initialize to an appropriate value
+            target.Run();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
