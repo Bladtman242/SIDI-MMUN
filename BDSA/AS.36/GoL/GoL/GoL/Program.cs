b170ebdac3aacfbfe4d03559d1239c09c894b6f2
Michael Storgaard <github@michaelstorgaard.com>
2012-09-06 13:44:22 +0200

    Added generator

--- BDSA/AS.36/GoL/GoL/GoL/Program.cs
+++ BDSA/AS.36/GoL/GoL/GoL/Program.cs
@@ -5,10 +5,67 @@ using System.Text;
 
 namespace GoL
 {
-    class Program
+    class GameOfLife
     {
+        private int?[,] Grid;
+
+        public GameOfLife(int size)
+        {
+            Grid = new int?[size, size];
+            GenerateState();
+
+            ConsoleKeyInfo cki;
+            do
+            {
+                cki = Console.ReadKey();
+                Console.WriteLine();
+                NextDay();
+            } while (cki.Key != ConsoleKey.Q);
+        }
+
+        private void GenerateState()
+        {
+            int size = Grid.GetLength(0);
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int? temp = random.Next(3);
+                    Grid[i, j] = (temp == 2 ? null : temp);
+                }
+            }
+        }
+
+        private void NextDay()
+        {
+            PrintGrid();
+        }
+
+        public void PrintGrid()
+        {
+            int size = Grid.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (Grid[i, j] == null)
+                    {
+                        Console.Write("z ");
+                    }
+                    else
+                    {
+                        Console.Write(Grid[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            GameOfLife gol = new GameOfLife(5);
         }
     }
 }
