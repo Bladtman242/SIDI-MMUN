dc846c07eac1cc660914945112ef824079e156f0
Michael Storgaard <github@michaelstorgaard.com>
2012-10-11 12:09:53 +0200

    added as.41

---
+++ BDSA/AS.41/Benchmarksystem/Benchmarksystem/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Benchmarksystem {
+    /// <summary>
+    /// Enumeration of different job states.
+    /// </summary>
+    enum State {
+        New, Submitted, Cancelled, Running, Terminated, Failed
+    }
+}
